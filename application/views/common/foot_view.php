6ff24b35220338a2433d641b7766bb63a1735859
Manuel Castillo <skpe829@gmail.com>
2019-04-23 20:23:31 -0500

    [NEW] comunes
    comunes

---
+++ application/views/common/foot_view.php
@@ -0,0 +1,7 @@
+</body>
+	<!-- jQuery -->
+	<script src="<?php echo base_url('vendor/jquery/jquery.min.js'); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+</html>
---
+++ application/views/common/head_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pico y Placa</title>
+
+	<!-- Bootstrap -->
+    <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    
+
+</head>
+<body>
