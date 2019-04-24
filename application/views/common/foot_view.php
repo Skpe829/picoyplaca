c6e4cb03422bb5f73ce0879db6e7613a88b18583
Manuel Castillo <skpe829@gmail.com>
2019-04-24 15:57:55 -0500

    [ADD] librerias bootstrap, jquery, bootstrap-material

--- application/views/common/foot_view.php
+++ application/views/common/foot_view.php
@@ -2,6 +2,16 @@
 	<!-- jQuery -->
 	<script src="<?php echo base_url('vendor/jquery/jquery.min.js'); ?>"></script>
 
-    <!-- Bootstrap Core JavaScript -->
+    <!-- Bootstrap JavaScript -->
     <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
+    <!-- JQuery UI -->
+    <script src="<?php echo base_url('vendor/jquery-ui/jquery-ui.min.js'); ?>"></script>
+
+    <script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
+    <script src="<?php echo base_url('vendor/bootstrap-material/js/bootstrap-material-datetimepicker.js'); ?>"></script>
+    
+
+	<!-- JavaScript Custom -->
+    <script src="<?php echo base_url('assets/picoYPlaca.js'); ?>"></script>
 </html>
--- application/views/common/head_view.php
+++ application/views/common/head_view.php
@@ -5,7 +5,11 @@
 
 	<!-- Bootstrap -->
     <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/bootstrap-material/css/bootstrap-material-datetimepicker.css'); ?>">
 
 </head>
 <body>
