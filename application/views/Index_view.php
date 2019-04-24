07dc82062c2756874ab3f57ba708d202e5b1cc7b
Manuel Castillo <skpe829@gmail.com>
2019-04-24 16:01:47 -0500

    [CHG] cambios en HTML

--- application/views/Index_view.php
+++ application/views/Index_view.php
@@ -4,19 +4,23 @@
 			Pico y Placa
 		</div>
 		<div class="card-body">
-			<div class="form-group">
-				<label>Placa del Vehiculo:</label>	
-				<input class="form-control" type="text" name="txtPlaca" id="txtPlaca" autocomplete="off" >
-			</div>
-			<div class="form-group">
-				<label>Fecha de Recorrido:</label>	
-				<input class="form-control" type="text" name="txtFecha" id="txtFecha" autocomplete="off">
-			</div>
-			<div class="form-group">
-				<label>Hora de Salida:</label>	
-				<input class="form-control" type="text" name="txtHora" id="txtHora" autocomplete="off">
-			</div>
-			<button class="btn btn-primary">Revisar</button>
+			<form id="frmSend">
+				<div class="form-group">
+					<label>Placa del Vehiculo:</label>	
+					<input class="form-control" type="text" name="txtPlaca" id="txtPlaca" autocomplete="off" maxlength="7">
+				</div>
+				<div class="form-group">
+					<label>Fecha de Recorrido:</label>	
+					<input class="form-control" type="text" name="txtFecha" id="txtFecha" autocomplete="off" readonly="">
+				</div>
+				<div class="form-group">
+					<label>Hora de Salida:</label>	
+					<input class="form-control" type="text" name="txtHora" id="txtHora" autocomplete="off" readonly="">
+				</div>
+				<button id="btnEnvio" class="btn btn-primary" disabled="">Revisar</button>
+			</form>
+			<hr>
+			<div id="divNtf"></div>
 		</div>
 	</div>
 </div>
