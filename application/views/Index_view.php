ca5c89fefb0dd4c64521cc6bb6d968c483e639ed
Manuel Castillo <skpe829@gmail.com>
2019-04-23 20:26:09 -0500

    [NEW] view: Index

---
+++ application/views/Index_view.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="card bg-light">
+		<div class="card-header">
+			Pico y Placa
+		</div>
+		<div class="card-body">
+			<div class="form-group">
+				<label>Placa del Vehiculo:</label>	
+				<input class="form-control" type="text" name="txtPlaca" id="txtPlaca" autocomplete="off" >
+			</div>
+			<div class="form-group">
+				<label>Fecha de Recorrido:</label>	
+				<input class="form-control" type="text" name="txtFecha" id="txtFecha" autocomplete="off">
+			</div>
+			<div class="form-group">
+				<label>Hora de Salida:</label>	
+				<input class="form-control" type="text" name="txtHora" id="txtHora" autocomplete="off">
+			</div>
+			<button class="btn btn-primary">Revisar</button>
+		</div>
+	</div>
+</div>
